fa1c3d0bc937cf27027be6d121b84261618d6f5d
Aleksandr Osipov <eltisprim@mail.ru>
2021-08-10 09:42:02 +0300

    Prepared Window

--- src/main/java/ru/geekbrains/lesson_7/hw/GameWindow.java
+++ src/main/java/ru/geekbrains/lesson_7/hw/GameWindow.java
@@ -4,11 +4,22 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame {
 
-    GameWindow() {
-        System.out.println("Hello! I'm window.");
+    private int winWidth = 800;  // задаем размеры окна,
+    private int winHeight = 600;  // размеры окна указываются в пикселях.
+    private int winPozX = 350;  // задаём начальную координату окна по оси X
+    private int winPozY = 100;  // задаём начальную координату окна по оси Y
 
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // вызываем метод дефолтной операции по закрытию окна
+    GameWindow() {
+        prepareWindow();
+
         setVisible(true); // вызываем метод открытия окна
     }
 
+    private void prepareWindow() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // вызываем метод дефолтной операции по закрытию окна
+        setLocation(winPozX, winPozY);  // вызываем метод создания окна с установленными размерами
+        setSize(winWidth, winHeight);  // вызываем метод позиционирования окна на экране
+        setTitle("Graphic Interface Game");  // выводим в окно Title
+        setResizable(false);  // запрещаем пользователю изменят размер окна.
+    }
 }
--- src/main/java/ru/geekbrains/lesson_7/hw/HomeWork_7.java
+++ src/main/java/ru/geekbrains/lesson_7/hw/HomeWork_7.java
@@ -2,7 +2,7 @@ package ru.geekbrains.lesson_7.hw;
 
 public class HomeWork_7 {
     public static void main(String[] args) {
-        GameWindow gameWindow = new GameWindow();
+        new GameWindow();
 
     }
 }
